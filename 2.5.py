4fbaee4e2fc2258e83967438c5d10252e8ba36e9
akshay4pec <120158466+akshay4pec@users.noreply.github.com>
2023-01-16 15:50:14 +0530

    Update 2.5.py

---
+++ 2.5.py
@@ -0,0 +1,8 @@
+"""Write a python program to check if the word “name” is present in the string 
+entered by the user (Print : “Yes” or “No”)."""
+a=str(input("please write anything "))
+if "name" in a:
+    print("yes")
+else:
+    print("no")
+    
